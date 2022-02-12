f109fa6f7959db30af4be60cf9638b6b19aef25d
SantiSL5 <santiagosolerllin@gmail.com>
2022-02-12 03:25:55 +0100

    secret

--- backend/onthewheels/settings.py
+++ backend/onthewheels/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from environ import Env
+
+env=Env()
+
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecurelocal-&syyk#ntb_5txgb_ib%rv6oul0h_e*s0wkiygcr0=@vz(z9&yt'
-
+SECRET_KEY = 'DJANGO_SECRET'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
